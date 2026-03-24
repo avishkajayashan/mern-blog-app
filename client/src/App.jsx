02e5bb1260a7137c6a46cb49806cacd66c37b431
Mona Lisa <avishkajayashand@gmail.com>
2026-03-24 13:20:18 +0530

    Setup frontend with Vite and Tailwind

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="p-5">
+      <h1 className="text-3xl font-bold text-red-500">
+        MERN Blog App 🚀
+      </h1>
+    </div>
+  );
+}
