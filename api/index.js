7dddde91b8b8da66541021822d01b569b015a86f
Mona Lisa <avishkajayashand@gmail.com>
2026-03-24 17:02:58 +0530

    Connected database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI).then(
+    () => {
+        console.log('MongoDB Connected Succesfully');
+    },
+).catch((err) => {
+    console.log('MongoDB Connection Failed');
+    console.error(err);
+});
 
 const app = express();
 
